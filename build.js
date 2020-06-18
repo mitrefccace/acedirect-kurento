163d5c1b66ed2480c3e882717424f07f31c7af1d
Ed Torres <ertorres@mitre.org>
2020-06-18 11:13:32 -0400

    updated for version v4.0

--- build.js
+++ build.js
@@ -24,23 +24,30 @@ function execCommand(cmd,wdir) {
 
 async function go() {
 
-  s = await execCommand('rm -rf node_modules >/dev/null  # removing node_modules','.');
+  s = await execCommand('rm ACEKurento-js/package-lock.json call-metrics/package-lock.json vendor/kurento-client-js/package-lock.json vendor/kurento-jsonrpc/package-lock.json vendor/reconnect-ws/package-lock.json package-lock.json  >/dev/null  # removing package lock files for regen ','.');
 
-  s = await execCommand('npm install >/dev/null  # main install','.');
+  s = await execCommand('rm -rf node_modules >/dev/null  # removing node_modules','.');
+  s = await execCommand('rm -rf vendor/kurento-client-js/node_modules >/dev/null  # removing node_modules','.');
+  s = await execCommand('rm -rf vendor/kurento-jsonrpc/node_modules >/dev/null  # removing node_modules','.');
+  s = await execCommand('rm -rf vendor/reconnect-ws/node_modules >/dev/null  # removing node_modules','.');
+
+  s = await execCommand('npm install   # main install','.');
 
   s = await execCommand('cp confs/jssip-modifications/RTCSession.js node_modules/jssip/lib-es5/.', '.');
   s = await execCommand('cp confs/jssip-modifications/UA.js node_modules/jssip/lib-es5/.', '.');
 
-  s = await execCommand('npm run bower >/dev/null ','.');
+  s = await execCommand('npm run bower  ','.');
 
   s = await execCommand('npm install  ','./vendor/kurento-client-js');
   s = await execCommand('npm install  ','./vendor/kurento-jsonrpc');
   s = await execCommand('npm install  ','./vendor/reconnect-ws');
 
-  console.log('TODO: configure these files: confs/kurento/WebRtcEndpoint.conf.ini, src/config/db.js, src/config/development.json');
-  console.log('TODO: npm run sequelize db:migrate');
-
-  console.log('RUNNING: npm run dev  OR  pm2 start process.json\n');
+  console.log('');
+  console.log('TODO:');
+  console.log('configure these files: confs/kurento/WebRtcEndpoint.conf.ini, src/config/db.js, src/config/development.json');
+  console.log('npm run sequelize db:migrate');
+  console.log('Run it: npm run dev  OR  pm2 start process.json\n');
+  console.log('');
 }
 
 go(); //MAIN
